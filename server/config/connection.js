342f647450cb876c917310f434d184cc406b9732
Brandon_Lopez <brandonchristopherlopez@gmail.com>
2023-11-22 20:08:47 -0800

    used activity as template vs mongo template

---
+++ server/config/connection.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+require('dotenv').config(); // REMOVE ON PRODUCTION
+
+mongoose.connect(process.env.MONGODB_URI);//|| 'mongodb://127.0.0.1:27017/tech-thoughts');
+
+module.exports = mongoose.connection;
